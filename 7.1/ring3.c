d4913f3f0d10e7c5be07f850df0958957b0f0dce
erlang-boy <liuhd3@lenovo.com>
2019-03-04 09:14:09 +0800

    next id

--- 7.1/ring3.c
+++ 7.1/ring3.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
+
 int 
 main(int argc, char **argv)
 {
@@ -11,6 +13,7 @@ main(int argc, char **argv)
   int fd[2];
   int error;
   int i;
+
   int nprocs;
 
   if ((argc != 2) || ((nprocs = atoi(argv[1])) <= 0)) {
@@ -35,6 +38,7 @@ main(int argc, char **argv)
     return 1;
   }
 
+  long procs[nprocs];
   for (i = 1; i < nprocs; i++) {
     if (pipe(fd) == -1) {
       fprintf(stderr, "[%ld]:failed to create pipe %d: %s\n", (long)getpid(), i, strerror(errno));
@@ -65,8 +69,30 @@ main(int argc, char **argv)
       break;
     } 
   }
+
+  procs[0] = (long)getpid();
+  char buf[BUF_SIZE] = {0};
+  char *nextID = NULL;
+  int readbytes = 0;
+
+  sprintf(buf, "%ld", (long)getpid());
+  write(STDOUT_FILENO, buf, strlen(buf)+1);
+
+
+  readbytes = read(STDIN_FILENO, buf, BUF_SIZE);
+  if (readbytes == -1) {
+    fprintf(stderr, "[%ld]: failed to read next id, proc: %d\n", (long)getpid(), i);
+    return 1;
+  }
+
+  procs[i] = atoi(buf);
   wait(NULL);
-  fprintf(stderr, "this is process %d with ID %ld and parent id %ld\n", i, (long)getpid(), (long)getppid());
+  for (i = 0; i< nprocs; i++)
+  {
+     sprintf(buf+i, "%ld ", procs[i]);
+  }
+
+  fprintf(stderr, "this is process %d with ID %ld and parent id %ld, buf: %s\n", i, (long)getpid(), (long)getppid(), buf);
   // wait(NULL);
   return 0;
 }
